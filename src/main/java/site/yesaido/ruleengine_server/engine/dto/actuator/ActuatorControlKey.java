188b5c90b04c8c985edaac154c6c4434708f128a
ysh011130 <ysh011130@naver.com>
2026-08-27 15:38:00 +0900

    fix: 액추에이터 제어가 어느 재배환경에 실행되었는지 확인을 위해 ActuatorControlKey에 @ToString 부착

--- src/main/java/site/yesaido/ruleengine_server/engine/dto/actuator/ActuatorControlKey.java
+++ src/main/java/site/yesaido/ruleengine_server/engine/dto/actuator/ActuatorControlKey.java
@@ -3,10 +3,12 @@ package site.yesaido.ruleengine_server.engine.dto.actuator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class ActuatorControlKey {
 
     private Long cultivationId;
